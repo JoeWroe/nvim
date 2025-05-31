624171adfac409b6918458f8cab9336943700161
Joe Wroe <jwroe@thoughtworks.com>
2025-05-31 08:26:46 +0100

    Enable autopairs on cmp confirm

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -48,3 +48,10 @@ cmp.setup({
   },
 })
 
+-- Enable autopairs on cmp confirm
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+
